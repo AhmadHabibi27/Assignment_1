5692fc3f89691ba48ea9cf58b9c481cdc654263b
AhmadHabibi27 <ahmadhabibi283@gmail.com>
2021-01-15 00:12:28 +0700

    Tugas ke 1

---
+++ nomor_1.py
@@ -0,0 +1,4 @@
+Nama = 'Habibi'
+Umur = 27
+Tinggi = 164.5
+print ('Nama saya %s, umur saya %s tahun dan tinggi saya %s cm' %(Nama,Umur,Tinggi))
---
+++ nomor_2.py
@@ -0,0 +1,4 @@
+r = int(7)
+phi = 22/7
+L = phi * (r * r)
+print('Luas lingakaran dengan jari-jari %s adalah %s cm2' %(r, L))
